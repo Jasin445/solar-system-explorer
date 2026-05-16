9a203b915be01a5ad46e4885989c16c143d40a9f
Jason Dagana <j.dagana@teflonhub.com>
2026-05-16 15:07:30 +0100

    cleanup on conflict resolution

--- src/components/ui/Label.jsx
+++ src/components/ui/Label.jsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva } from "class-variance-authority"
-import { cn } from "../../lib/utils"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -10,7 +9,7 @@ const labelVariants = cva(
 const Label = React.forwardRef(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={labelVariants(), className}
     {...props}
   />
 ))
