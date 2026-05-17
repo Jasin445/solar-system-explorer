d0699893555c9a1f113affb57edee2098aace5e5
AlexanderHotonu <alexanderhotonu1@gmail.com>
2026-05-17 21:34:41 +0100

    Describe your changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
-
-const Header = () => {
+const NavBar = () => {
   return (
-    <header>Header</header>
+     <header>
+        <div className="header-wrapper">
+          <img
+            src="https://res.cloudinary.com/jasond/image/upload/v1770026324/graphics_fcihct.png"
+            width="121px"
+            height="52px"
+            alt="logo"
+          />
+        </div>
+      </header>
   )
 }
 
-export default Header
+export default NavBar
